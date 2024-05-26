33afb2ad3014e9fc521424fa05433a775da95310
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-05-26 04:21:52 +0400

    Assets

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
-import Services from "./components/services/Sidebar";
+import Services from "./components/services/Services";
 import Resume from "./components/resume/Resume";
 import Portfolio from "./components/portfolio/Portfolio";
 import Pricing from "./components/pricing/Pricing";
